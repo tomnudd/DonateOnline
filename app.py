b828b09f9d80fd2842c8f3d1957679520aff4e04
Tom <thomas.g.nudd@durham.ac.uk>
2020-03-29 02:21:51 +0100

    skeleton login code from flask_dance docs, adapted for modern api

---
+++ app.py
@@ -0,0 +1,28 @@
+import os 
+
+GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
+GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
+APP_SECRET = os.environ["APP_SECRET"]
+
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+app.secret_key = "supersekrit"
+blueprint = make_google_blueprint(
+    client_id = GOOGLE_CLIENT_ID,
+    client_secret = GOOGLE_CLIENT_SECRET,
+    scope = ["profile", "email"]
+)
+app.register_blueprint(blueprint, url_prefix = "/login")
+
+@app.route("/")
+def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v3/userinfo")
+    assert resp.ok, resp.text
+    return resp.json()
+
+if __name__ == "__main__":
+    app.run()
